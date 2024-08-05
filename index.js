091c84415062bbd3a55d0b16c4d7bdd252b39b38
Abhijith Lenin <abhijithlenin180@gmail.com>
2024-08-05 23:48:21 +0530

    jsx intro

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import ReactDOM from "react-dom/client"
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render("Hai") 
+import ReactDOM from "react-dom/client";
+
+const header = (
+	<h1 className="header" tabIndex="1">
+		Welcome
+	</h1>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
