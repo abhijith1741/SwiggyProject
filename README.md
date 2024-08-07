ec58fb30dcb46ad1e7003933218dd111e246f180
Abhijith Lenin <abhijithlenin180@gmail.com>
2024-08-07 23:55:10 +0530

    initial food app commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- component composition -->
+    - A component which inside a component called component composition
+
+<!-- Bundler -->
+    - Which bundle our code and render in the browser DOM
+    - common bundlers are [Parcel, Vite, webpack]
+    
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 
-const header = (
-	<h1 className="header" tabIndex="1">
-		Welcome
-	</h1>
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+const Header = () => {
+	return (
+		<div className="header">
+			<div className="logo-container">
+				<img className="logo" src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg" alt="" />
+			</div>
+			<div className="nav-item">
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Contact Us</li>
+					<li>Cart</li>
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+const AppLayout = () => {
+	return (
+		<div className="app">
+			<Header />
+		</div>
+	)
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout />)
